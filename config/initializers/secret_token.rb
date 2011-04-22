6bfe0a1916ec50a350614041ea376fa0c5fcb5e5
Bradley Spaulding <bspaulding@dataxu.com>
2011-04-22 14:53:47 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurrencyTracker::Application.config.secret_token = '6725af6ed78f3b1f346be407ac50359be53dc023b31fb1d673cac36c6bc1681719c93455b1dcdcfe6ebcd37b3495781b42507994dcbef009f89efcd95249b08b'
